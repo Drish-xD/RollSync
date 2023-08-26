0ef987fc6151c7423ffd5ea79837faa8f54d529c
Drish-xD <drish.xd@gmail.com>
2023-08-26 18:29:33 +0530

    added validation

--- src/components/auth/Form.tsx
+++ src/components/auth/Form.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { Checkbox, Input, Link, Spacer } from '@nextui-org/react';
 import NextLink from 'next/link';
 import { RefObject, useState } from 'react';
@@ -7,6 +5,20 @@ import { Eye, EyeOff } from 'react-feather';
 
 export default function Form({ formRef }: { formRef: RefObject<HTMLFormElement> }) {
   const [isVisible, setIsVisible] = useState(false);
+  const [valid, setValid] = useState<{ [key: string]: boolean | undefined }>({
+    email: undefined,
+    password: undefined
+  });
+
+  const validateEmail = (value: string) => /^[a-z]{2}\d{4}$/.test(value);
+
+  const handleValidation = () => {
+    const email = formRef.current!['email'].value;
+    const password = formRef.current!['password'].value;
+    const isValidEmail = validateEmail(email);
+    const isValidPassword = password.length > 6;
+    setValid({ email: isValidEmail, password: isValidPassword });
+  };
 
   return (
     <form ref={formRef}>
@@ -15,10 +27,13 @@ export default function Form({ formRef }: { formRef: RefObject<HTMLFormElement> 
         label="Email"
         name="email"
         placeholder="Enter your email"
-        description="without '@srmist.edu.in"
+        description="only Email Prefix (without @srmist.edu.in)"
         fullWidth
         isRequired
+        errorMessage={valid.email === false && 'Invalid email'}
+        validationState={valid.email === false ? 'invalid' : 'valid'}
         endContent={<p className="pointer-events-none flex items-center">@srmist.edu.in</p>}
+        onBlur={handleValidation}
       />
       <Spacer y={5} />
       <Input
@@ -35,10 +50,15 @@ export default function Form({ formRef }: { formRef: RefObject<HTMLFormElement> 
           </button>
         }
         type={isVisible ? 'text' : 'password'}
+        errorMessage={valid.password === false && 'Invalid Password'}
+        validationState={valid.password === false ? 'invalid' : 'valid'}
+        description="Password length > 6"
         fullWidth
         isRequired
+        onBlur={handleValidation}
       />
       <Spacer y={2} />
+
       <div className="flex justify-between">
         <Checkbox defaultSelected radius="full">
           Remember me
